8bcda61e7097b932a2219261a52c7ee3ba82d7b8
Tony <89054896+SteveBartmoss@users.noreply.github.com>
2025-04-02 09:06:35 -0600

    feat: Se agrego un archivo para el componente de las portadas en el perfil del usuario

---
+++ src/ui/coverPage/CoverPage.jsx
@@ -0,0 +1,16 @@
+
+
+export function CoverPage({backgroundImage,imgPerfil,title,text}){
+    <>
+        <style>
+            {
+                `.img-portada{
+                    background: url(${backgroundImage}) no-repeat center center/cover;
+                }`
+            }
+        </style>
+        <section>
+            
+        </section>
+    </>
+}

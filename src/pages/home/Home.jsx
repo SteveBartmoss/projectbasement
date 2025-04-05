188429bfb76a0d411bad4f28130b7016d3d6bba8
steve <za19012397@zapopan.tecmm.edu.mx>
2025-04-05 14:25:19 -0600

    feat: Se agrego el componente para la pagina principal de la aplicacion

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import './home.css'
+
+export function Home(){
+    return(
+        <div>
+            <div>
+                <h1>Aside izquierdo</h1>
+            </div>
+            <div>
+                <h1>Main reel</h1>
+            </div>
+            <div>
+                <h1>Aside derecho</h1>
+            </div>
+        </div>
+    )
+}

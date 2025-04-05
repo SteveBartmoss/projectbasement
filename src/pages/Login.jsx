022727db032ca0f95768938fbe8e465f6119bb8d
steve <za19012397@zapopan.tecmm.edu.mx>
2025-04-05 14:17:11 -0600

    feat: Se agregaron elementos y estructura para la pagain del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,25 @@
-import { DivRow } from "../ui/containers/Containers";
+import { Btn } from "../ui/btn/Btn";
+import { DivCol, DivRow } from "../ui/containers/Containers";
+import { TextField } from "../ui/textField/TextField";
 import './login.css'
 
 export function Login() {
     return (
         <DivRow>
             <img className="img-login" src="/assets/fogforest.png" />
+            <DivCol>
+                <div>
+                    <h1>Bienvenido a Basement</h1>
+
+                    <p>Inicia sesion para compartir con los demas</p>
+
+                    <TextField />
+
+                    <TextField />
+
+                    <Btn>Iniciar sesion</Btn>
+                </div>
+            </DivCol>
         </DivRow>
     )
 }
